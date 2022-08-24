ec0a2c956894ce3d89fa9c294ff469537a83fab1
Nghi2001 <duynghikhum9@gmail.com>
2022-08-24 11:57:29 +0700

    change hashTable

--- data-structure/Linked-List.js
+++ data-structure/Linked-List.js
@@ -26,9 +26,66 @@ class LinkedList {
         }
         this.size ++;
     }
+
+    insertAt( element, index) {
+        if( index < 0 || index > this.size) {
+            return 'invalid index'
+        } else {
+            let node = new Node(element);
+            let curr,prev;
+            curr = this.head;
+            if(index == 0) {
+                node.next = this.head;
+                this.head = node
+            } else {
+                curr = this.head;
+                let it = 0;
+                while (it < index) {
+                    it++;
+                    prev = curr;
+                    curr = curr.next;
+                }
+                node.next = curr;
+                prev.next = node;
+                // console.log(prev);
+            }
+            this.size++
+        }
+    }
+    printListData() {
+        let current = this.head;
+        while(current) {
+            console.log(current.element);
+            current = current.next;
+        }
+    }
 }
 
+function reverse(node) {
+    let current = node;
+    let next = null;
+    let prev = null;
+    while(current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+        console.log(prev);
+    }
+    node = prev;
+    // console.log(node);
+}
+/**
+ * current = head; next = null; prev = null
+ * chạy while
+ * next = current.next => 3
+ */
 let list = new LinkedList()
 list.add(new Node(1))
 list.add(new Node(2));
-console.log(list);
+list.add(new Node(4));
+
+list.insertAt(new Node(3),1)
+list.printListData();
+
+reverse(list.head);
